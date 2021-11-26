f0845bb52466efa0b34acfc41a65f056e7f32156
Leo Yang <junjiey3@illinois.edu>
2021-11-25 20:56:31 -0800

    improving lda...

---
+++ src/lda2.py
@@ -0,0 +1,130 @@
+# credit: https://radimrehurek.com/gensim/auto_examples/tutorials/run_lda.html#sphx-glr-download-auto-examples-tutorials-run-lda-py
+# ldamodel: https://radimrehurek.com/gensim/models/ldamodel.html
+
+import logging
+import numpy as np
+
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+docs = [] # corpus, where each string is a document
+
+with open('textretrieval.txt', 'r') as fin:
+    for line in fin:
+        docs.append(line.strip())
+
+print(len(docs))
+#print(docs[0])
+
+doc_num = 2
+
+docs = docs[doc_num:doc_num+1]
+
+from nltk.tokenize import RegexpTokenizer
+
+# Split the documents into tokens.
+tokenizer = RegexpTokenizer(r'\w+')
+for idx in range(len(docs)):
+    docs[idx] = docs[idx].lower()  # Convert to lowercase.
+    docs[idx] = tokenizer.tokenize(docs[idx])  # Split into words.
+
+# Remove numbers, but not words that contain numbers.
+docs = [[token for token in doc if not token.isnumeric()] for doc in docs]
+
+# Remove words that are only one character.
+docs = [[token for token in doc if len(token) > 1] for doc in docs]
+
+# Lemmatize the documents.
+from nltk.stem.wordnet import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+docs = [[lemmatizer.lemmatize(token) for token in doc] for doc in docs]
+
+# Compute bigrams.
+from gensim.models import Phrases
+
+# Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+bigram = Phrases(docs, min_count=2)
+for idx in range(len(docs)):
+    for token in bigram[docs[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to document.
+            docs[idx].append(token)
+
+print(docs[0])
+
+# Remove rare and common tokens.
+from gensim.corpora import Dictionary
+
+# Create a dictionary representation of the documents.
+dictionary = Dictionary(docs)
+
+# Filter out words that occur less than 20 documents, or more than 50% of the documents.
+dictionary.filter_extremes(no_below=1, no_above=1)
+
+from gensim.parsing.preprocessing import STOPWORDS
+import nltk
+from nltk.corpus import stopwords
+
+my_stop_words = STOPWORDS.union(set(stopwords.words('english')))
+
+with open('stopwords.txt', 'w') as fout:
+    fout.write(str(my_stop_words))
+
+del_ids = [k for k,v in dictionary.items() if v in my_stop_words or len(v) == 1]
+
+# remove unwanted word ids from the dictionary in place
+dictionary.filter_tokens(bad_ids=del_ids)
+
+# Bag-of-words representation of the documents.
+corpus = [dictionary.doc2bow(doc) for doc in docs]
+
+print('Number of unique tokens: %d' % len(dictionary))
+print('Number of documents: %d' % len(corpus))
+
+# Train LDA model.
+from gensim.models import LdaModel
+
+# Set training parameters.
+num_topics = 1 * 7
+chunksize = 1 # how many documents to process at a time
+passes = 100 # epochs
+iterations = 400
+eval_every = 1  # Don't evaluate model perplexity, takes too much time.
+
+# Make a index to word dictionary.
+temp = dictionary[0]  # This is only to "load" the dictionary.
+id2word = dictionary.id2token
+
+model = LdaModel(
+    corpus=corpus,
+    id2word=id2word,
+    chunksize=chunksize,
+    alpha='auto',
+    eta='auto',
+    iterations=iterations,
+    num_topics=num_topics,
+    passes=passes,
+    eval_every=eval_every
+)
+
+#top_topics = model.top_topics(corpus) #, num_words=20)
+top_topics = model.get_document_topics(corpus[0]) # [(topic_id, prob)]
+
+# Average topic coherence is the sum of topic coherences of all topics, divided by the number of topics.
+# avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+# print('Average topic coherence: %.4f.' % avg_topic_coherence)
+
+from pprint import pprint
+pprint(top_topics)
+
+i = 1
+for topic in top_topics:
+    print('topic {}'.format(i))
+    i+=1
+    topic_term_distribution = model.get_topic_terms(topic[0], topn=40)
+    for term in topic_term_distribution:
+        print('{}, {}'.format(dictionary[term[0]], term[1]))
+    print('prob: {}'.format(topic[-1]))
+
+with open('tokens.txt', 'w') as fout:
+    fout.write(str(dictionary.token2id))
